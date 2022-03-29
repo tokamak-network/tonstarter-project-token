dc2adf7252f85178a284592618c3d095cd31656c
SEFI2 <kadirpili@gmail.com>
2022-03-29 12:55:31 +0600

    Add script templtates to execeute

---
+++ scripts/token-dividend-pool/dao-agendas.sh
@@ -0,0 +1,3 @@
+npx hardhat create-set-seig-manager-agenda --network rinkeby --dao-agenda-manager-address TODO --deposit-manager-address 0x57F5CD759A5652A697D539F1D9333ba38C615FC2 --seig-manager-address 0x957DaC3D3C4B82088A4939BE9A8063e20cB2efBE
+npx hardhat create-set-power-ton-agenda --network rinkeby --power-ton-address TODO --dao-agenda-manager-address TODO --seig-manager-address 0x957DaC3D3C4B82088A4939BE9A8063e20cB2efBE
+npx hardhat execute-agenda --network rinkeby --ageda-id TODO --dao-agenda-manager-address TODO
--- scripts/token-dividend-pool/deploy-contracts.sh
+++
@@ -1,3 +0,0 @@
-npx hardhat deploy-erc20-recorder --network rinkeby --deposit-manager-address 0x57F5CD759A5652A697D539F1D9333ba38C615FC2 --owner-address
-npx hardhat deploy-token-dividend-pool --network rinkeby  --erc20-recorder-address TODO
-npx hardhat deploy-power-ton-swapper --network rinkeby --tos-address TODO --erc20-recorder-address TODO --wton-address 0x709bef48982Bbfd6F2D4Be24660832665F53406C --uniswap-router-address 0xe592427a0aece92de3edee1f18e0157c05861564 --layer2-registry-address 0xA609Cb2b9b0A4845077D2C965B7C6DFE5F59c847 --seig-manager-address 0x957DaC3D3C4B82088A4939BE9A8063e20cB2efBE
