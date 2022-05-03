58ca45c1ba0ea2e2e7589afabe3f56acd1718db4
zena <zena.p@onther.io>
2022-05-03 09:19:19 +0900

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -11,7 +11,12 @@ module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
-      chainId: 31337,
+      chainId: 31337
+    },
+    local: {
+      url: `http://127.0.0.1:8545/`,
+      accounts: [`${process.env.PRIVATE_KEY}`,`${process.env.PRIVATE_KEY_2}`,`${process.env.PRIVATE_KEY_3}`,],
+      timeout: 100000000,
     },
     rinkeby: {
       url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
