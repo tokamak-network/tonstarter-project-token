6b5a7dd05e3372a2eb3ef495fcc8758f4daa9f93
zena <zena.p@onther.io>
2022-05-23 12:56:56 +0900

    mod. projectToken name, symbol

--- scripts/deploy/4_deploy_ProjectTokenProxy.js
+++ scripts/deploy/4_deploy_ProjectTokenProxy.js
@@ -1,35 +1,35 @@
 const hre = require("hardhat");
-require('dotenv').config()
-const {
-  keccak256,
-} = require("web3-utils");
+require("dotenv").config();
+const { keccak256 } = require("web3-utils");
 
 async function main() {
   const [admin] = await hre.ethers.getSigners();
 
-  const ProjectTokenProxy = await hre.ethers.getContractFactory("ProjectTokenProxy");
-  const projectTokenProxy = await ProjectTokenProxy.deploy("ProjectToken","Starter");
+  const ProjectTokenProxy = await hre.ethers.getContractFactory(
+    "ProjectTokenProxy"
+  );
+  const projectTokenProxy = await ProjectTokenProxy.deploy(
+    "TOS Project NFT",
+    "TosPNT"
+  );
 
-  let tx = await projectTokenProxy.deployed();
+  const tx = await projectTokenProxy.deployed();
 
   console.log("tx:", tx.deployTransaction.hash);
   console.log("projectTokenProxy deployed to:", projectTokenProxy.address);
 
+  await (await projectTokenProxy.connect(admin).upgradeTo("로직주소")).wait();
 
-  await (await projectTokenProxy.connect(admin).upgradeTo("0x63e559c405BffC9507e109654c28BC283Cd6055C")).wait();
-
-
-  //projectTokenProxy deployed to: 0x0525e7040c71fb6692324AA270788DEa41Eb0129
+  // projectTokenProxy deployed to: 0x0525e7040c71fb6692324AA270788DEa41Eb0129
   await run("verify", {
     address: projectTokenProxy.address,
-    constructorArgsParams: ["ProjectToken","Starter"],
+    constructorArgsParams: ["TOS Project NFT", "TosPNT"],
   });
-
 }
 
 main()
   .then(() => process.exit(0))
-  .catch(error => {
+  .catch((error) => {
     console.error(error);
     process.exit(1);
   });
--- tasks/token-factory-deploy.js
+++ tasks/token-factory-deploy.js
@@ -1,175 +1,237 @@
-const fs = require('fs');
+const fs = require("fs");
 
-const {
-    keccak256,
-  } = require("web3-utils");
+const { keccak256 } = require("web3-utils");
 
+task("deploy-token-factory", "Deploy tokenFactory").setAction(
+  async function ({}) {
+    const [admin] = await ethers.getSigners();
 
-task("deploy-token-factory", "Deploy tokenFactory")
-    .setAction(async function ({ }) {
-        const [admin] = await ethers.getSigners();
+    await hre.ethers.provider.send("hardhat_setBalance", [
+      admin.address,
+      "0x56BC75E2D63100000",
+    ]);
 
-        // await hre.ethers.provider.send("hardhat_setBalance", [
-        //     admin.address,
-        //     "0x56BC75E2D63100000",
-        // ]);
+    const ERC20SimpleFactory = await ethers.getContractFactory(
+      "ERC20SimpleFactory"
+    );
+    const erc20SimpleFactory = await ERC20SimpleFactory.connect(admin).deploy();
+    await erc20SimpleFactory.deployed();
 
-        const ERC20AFactory = await ethers.getContractFactory("ERC20AFactory");
-        let erc20AFactory = await ERC20AFactory.connect(admin).deploy();
-        await erc20AFactory.deployed();
+    console.log("ERC20SimpleFactory Deployed:", erc20SimpleFactory.address);
 
-        console.log("ERC20AFactory Deployed:", erc20AFactory.address);
-
-        await run("verify", {
-            address: erc20AFactory.address,
-            constructorArgsParams: [],
-        });
-  });
-
+    await run("verify", {
+      address: erc20SimpleFactory.address,
+      constructorArgsParams: [],
+    });
+  }
+);
 
 task("deploy-project-token", "Deploy projectToken")
-    .addParam("name", "Name")
-    .addParam("symbol", "Symbol")
-    .setAction(async function ({name, symbol}) {
-        const [admin] = await ethers.getSigners();
+  .addParam("name", "Name")
+  .addParam("symbol", "Symbol")
+  .setAction(async function ({ name, symbol }) {
+    const [admin] = await ethers.getSigners();
 
-        const ProjectToken = await ethers.getContractFactory("ProjectToken");
-        let projectToken = await ProjectToken.connect(admin).deploy(name, symbol);
-        await projectToken.deployed();
+    const ProjectToken = await ethers.getContractFactory("ProjectToken");
+    const projectToken = await ProjectToken.connect(admin).deploy(name, symbol);
+    await projectToken.deployed();
 
-        console.log("ProjectToken Deployed:", projectToken.address);
+    console.log("ProjectToken Deployed:", projectToken.address);
 
-        const ProjectTokenProxy = await ethers.getContractFactory("ProjectTokenProxy");
-        const projectTokenProxy = await ProjectTokenProxy.connect(admin).deploy(name, symbol);
-        await projectTokenProxy.deployed();
+    const ProjectTokenProxy = await ethers.getContractFactory(
+      "ProjectTokenProxy"
+    );
+    const projectTokenProxy = await ProjectTokenProxy.connect(admin).deploy(
+      name,
+      symbol
+    );
+    await projectTokenProxy.deployed();
 
-        console.log("ProjectTokenProxy Deployed:", projectTokenProxy.address);
-        await (await projectTokenProxy.connect(admin).upgradeTo(projectToken.address)).wait();
+    console.log("ProjectTokenProxy Deployed:", projectTokenProxy.address);
 
+    await (
+      await projectTokenProxy.connect(admin).upgradeTo(projectToken.address)
+    ).wait();
 
-        await run("verify", {
-            address: projectToken.address,
-            constructorArgsParams: [name, symbol],
-        });
+    await run("verify", {
+      address: projectToken.address,
+      constructorArgsParams: [name, symbol],
+    });
 
-        await run("verify", {
-            address: projectTokenProxy.address,
-            constructorArgsParams: [name, symbol],
-        });
+    await run("verify", {
+      address: projectTokenProxy.address,
+      constructorArgsParams: [name, symbol],
+    });
   });
 
-
-task("change-owner-token-and-project", "Change the owner of TokenFactory and ProjectToken")
+task(
+  "change-owner-token-and-project",
+  "Change the owner of TokenFactory and ProjectToken"
+)
   .addParam("tokenFactoryAddress", "TokenFactory Address")
   .addParam("projectTokenAddress", "ProjectToken Address")
   .addParam("newOwnerAddress", "New Owner Address")
-  .setAction(async function ({tokenFactoryAddress, projectTokenAddress, newOwnerAddress}) {
-        const [admin] = await ethers.getSigners();
+  .setAction(async function ({
+    tokenFactoryAddress,
+    projectTokenAddress,
+    newOwnerAddress,
+  }) {
+    const [admin] = await ethers.getSigners();
 
+    const ERC20SimpleFactoryABI = JSON.parse(
+      await fs.readFileSync("./abi/ERC20SimpleFactory.json")
+    ).abi;
+    const ProjectTokenProxyABI = JSON.parse(
+      await fs.readFileSync("./abi/ProjectTokenProxy.json")
+    ).abi;
+    const ProjectTokenABI = JSON.parse(
+      await fs.readFileSync("./abi/ProjectToken.json")
+    ).abi;
 
-        const ERC20AFactoryABI = JSON.parse(await fs.readFileSync("./abi/ERC20AFactory.json")).abi;
-        const ProjectTokenProxyABI = JSON.parse(await fs.readFileSync("./abi/ProjectTokenProxy.json")).abi;
-        const ProjectTokenABI = JSON.parse(await fs.readFileSync("./abi/ProjectToken.json")).abi;
+    const erc20SimpleFactory = new ethers.Contract(
+      tokenFactoryAddress,
+      ERC20SimpleFactoryABI,
+      ethers.provider
+    );
 
-        const erc20AFactory = new ethers.Contract(
-            tokenFactoryAddress,
-            ERC20AFactoryABI,
-            ethers.provider
-        );
+    const DEFAULT_ADMIN_ROLE =
+      "0x0000000000000000000000000000000000000000000000000000000000000000";
 
-        let DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
-        let tx = await erc20AFactory.connect(admin).grantRole(DEFAULT_ADMIN_ROLE, newOwnerAddress);
-        await tx.wait();
-        console.log("ERC20AFactory grantRole(DEFAULT_ADMIN_ROLE) to newOwnerAddress:  tx:", tx.hash);
+    const tx = await erc20SimpleFactory
+      .connect(admin)
+      .grantRole(DEFAULT_ADMIN_ROLE, newOwnerAddress);
 
-        let hasRole = await erc20AFactory.connect(admin).hasRole(DEFAULT_ADMIN_ROLE, newOwnerAddress);
-        console.log("ERC20AFactory newOwnerAddress hasRole(DEFAULT_ADMIN_ROLE) :", hasRole);
+    await tx.wait();
+    console.log(
+      "ERC20SimpleFactory grantRole(DEFAULT_ADMIN_ROLE) to newOwnerAddress:  tx:",
+      tx.hash
+    );
 
-        const projectTokenProxy = new ethers.Contract(
-            projectTokenAddress,
-            ProjectTokenProxyABI,
-            ethers.provider
-        );
+    const hasRole = await erc20SimpleFactory
+      .connect(admin)
+      .hasRole(DEFAULT_ADMIN_ROLE, newOwnerAddress);
+    console.log(
+      "ERC20SimpleFactory newOwnerAddress hasRole(DEFAULT_ADMIN_ROLE) :",
+      hasRole
+    );
 
-        let tx1 = await projectTokenProxy.connect(admin).transferOwnership(newOwnerAddress);
-        await tx1.wait();
-        console.log("ProjectTokenProxy transferOwnership to newOwnerAddress : tx:", tx1.hash);
+    const projectTokenProxy = new ethers.Contract(
+      projectTokenAddress,
+      ProjectTokenProxyABI,
+      ethers.provider
+    );
 
-        let newOwner = await projectTokenProxy.connect(admin)._owner();
-        console.log("ProjectTokenProxy _owner  :", newOwner);
+    const tx1 = await projectTokenProxy
+      .connect(admin)
+      .transferOwnership(newOwnerAddress);
+    await tx1.wait();
+    console.log(
+      "ProjectTokenProxy transferOwnership to newOwnerAddress : tx:",
+      tx1.hash
+    );
 
+    const newOwner = await projectTokenProxy.connect(admin)._owner();
+    console.log("ProjectTokenProxy _owner  :", newOwner);
 
-        const projectToken = new ethers.Contract(
-            projectTokenAddress,
-            ProjectTokenABI,
-            ethers.provider
-        );
+    const projectToken = new ethers.Contract(
+      projectTokenAddress,
+      ProjectTokenABI,
+      ethers.provider
+    );
 
-        let isOwner = await projectToken.connect(admin).isOwner(admin.address);
-        console.log("ProjectTokenProxy isOwner(",admin.address,") :", isOwner);
-});
+    const isOwner = await projectToken.connect(admin).isOwner(admin.address);
+    console.log("ProjectTokenProxy isOwner(", admin.address, ") :", isOwner);
+  });
 
-
-task("display-owner-token-and-project", "Display the owner of TokenFactory and ProjectToken")
+task(
+  "display-owner-token-and-project",
+  "Display the owner of TokenFactory and ProjectToken"
+)
   .addParam("tokenFactoryAddress", "TokenFactory Address")
   .addParam("projectTokenAddress", "ProjectToken Address")
   .addParam("ownerAddress", "Owner Address")
-  .setAction(async function ({tokenFactoryAddress, projectTokenAddress, ownerAddress}) {
-        const [admin] = await ethers.getSigners();
-        let DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  .setAction(async function ({
+    tokenFactoryAddress,
+    projectTokenAddress,
+    ownerAddress,
+  }) {
+    const [admin] = await ethers.getSigners();
+    const DEFAULT_ADMIN_ROLE =
+      "0x0000000000000000000000000000000000000000000000000000000000000000";
 
-        const ERC20AFactoryABI = JSON.parse(await fs.readFileSync("./abi/ERC20AFactory.json")).abi;
-        const ProjectTokenProxyABI = JSON.parse(await fs.readFileSync("./abi/ProjectTokenProxy.json")).abi;
-        const ProjectTokenABI = JSON.parse(await fs.readFileSync("./abi/ProjectToken.json")).abi;
+    const ERC20SimpleFactoryABI = JSON.parse(
+      await fs.readFileSync("./abi/ERC20SimpleFactory.json")
+    ).abi;
+    const ProjectTokenProxyABI = JSON.parse(
+      await fs.readFileSync("./abi/ProjectTokenProxy.json")
+    ).abi;
+    const ProjectTokenABI = JSON.parse(
+      await fs.readFileSync("./abi/ProjectToken.json")
+    ).abi;
 
-        const erc20AFactory = new ethers.Contract(
-            tokenFactoryAddress,
-            ERC20AFactoryABI,
-            ethers.provider
-        );
+    const erc20SimpleFactory = new ethers.Contract(
+      tokenFactoryAddress,
+      ERC20SimpleFactoryABI,
+      ethers.provider
+    );
 
-        let hasRole = await erc20AFactory.connect(admin).hasRole(DEFAULT_ADMIN_ROLE, ownerAddress);
-        console.log("ERC20AFactory ownerAddress hasRole(DEFAULT_ADMIN_ROLE) :", hasRole);
+    const hasRole = await erc20SimpleFactory
+      .connect(admin)
+      .hasRole(DEFAULT_ADMIN_ROLE, ownerAddress);
+    console.log(
+      "ERC20SimpleFactory ownerAddress hasRole(DEFAULT_ADMIN_ROLE) :",
+      hasRole
+    );
 
-        const projectTokenProxy = new ethers.Contract(
-            projectTokenAddress,
-            ProjectTokenProxyABI,
-            ethers.provider
-        );
+    const projectTokenProxy = new ethers.Contract(
+      projectTokenAddress,
+      ProjectTokenProxyABI,
+      ethers.provider
+    );
 
-        let _owner = await projectTokenProxy.connect(admin)._owner();
-        console.log("ProjectTokenProxy _owner  :", _owner);
+    const _owner = await projectTokenProxy.connect(admin)._owner();
+    console.log("ProjectTokenProxy _owner  :", _owner);
 
+    const projectToken = new ethers.Contract(
+      projectTokenAddress,
+      ProjectTokenABI,
+      ethers.provider
+    );
 
-        const projectToken = new ethers.Contract(
-            projectTokenAddress,
-            ProjectTokenABI,
-            ethers.provider
-        );
-
-        let isOwner = await projectToken.connect(admin).isOwner(ownerAddress);
-        console.log("ProjectTokenProxy isOwner(",ownerAddress,") :", isOwner);
-
-});
-
+    const isOwner = await projectToken.connect(admin).isOwner(ownerAddress);
+    console.log("ProjectTokenProxy isOwner(", ownerAddress, ") :", isOwner);
+  });
 
 task("renounce-owner-token", "renounce the owner of TokenFactory")
   .addParam("tokenFactoryAddress", "TokenFactory Address")
-  .setAction(async function ({tokenFactoryAddress }) {
-        const [admin] = await ethers.getSigners();
-        let DEFAULT_ADMIN_ROLE = "0x0000000000000000000000000000000000000000000000000000000000000000";
+  .setAction(async function ({ tokenFactoryAddress }) {
+    const [admin] = await ethers.getSigners();
+    const DEFAULT_ADMIN_ROLE =
+      "0x0000000000000000000000000000000000000000000000000000000000000000";
 
-        const ERC20AFactoryABI = JSON.parse(await fs.readFileSync("./abi/ERC20AFactory.json")).abi;
+    const ERC20SimpleFactoryABI = JSON.parse(
+      await fs.readFileSync("./abi/ERC20SimpleFactory.json")
+    ).abi;
 
-        const erc20AFactory = new ethers.Contract(
-            tokenFactoryAddress,
-            ERC20AFactoryABI,
-            ethers.provider
-        );
+    const erc20SimpleFactory = new ethers.Contract(
+      tokenFactoryAddress,
+      ERC20SimpleFactoryABI,
+      ethers.provider
+    );
 
-        let tx = await erc20AFactory.connect(admin).renounceRole(DEFAULT_ADMIN_ROLE, admin.address);
-        console.log("ERC20AFactory renounceRole :", tx.hash);
+    const tx = await erc20SimpleFactory
+      .connect(admin)
+      .renounceRole(DEFAULT_ADMIN_ROLE, admin.address);
+    console.log("ERC20SimpleFactory renounceRole :", tx.hash);
 
-        let hasRole = await erc20AFactory.connect(admin).hasRole(DEFAULT_ADMIN_ROLE, admin.address);
-        console.log("ERC20AFactory ",admin.address," hasRole(DEFAULT_ADMIN_ROLE) :", hasRole);
-});
+    const hasRole = await erc20SimpleFactory
+      .connect(admin)
+      .hasRole(DEFAULT_ADMIN_ROLE, admin.address);
+
+    console.log(
+      "ERC20SimpleFactory ",
+      admin.address,
+      " hasRole(DEFAULT_ADMIN_ROLE) :",
+      hasRole
+    );
+  });
